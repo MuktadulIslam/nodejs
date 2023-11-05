d7f4b9d91bb77490fcb744ecb9c5a91a472ca340
Muktadul Islam <76801029+MuktadulIslam@users.noreply.github.com>
2023-11-05 20:04:23 +0600

    aa

--- index.js
+++ index.js
@@ -1,12 +1,11 @@
-const http = require('http');
-const PORT = 3000;
+const express = require('express');
+const app = express();
+const port = 3000;
 
-const server = http.createServer((req, res) => {
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/plain');
-  res.end('My name is Muktadul');
+app.get('/', (req, res) => {
+  res.send('Hello, World! by Muktadul Islam');
 });
 
-server.listen(PORT, () => {
-  console.log(`Server running at http://localhost:${PORT}/`);
+app.listen(port, () => {
+  console.log(`Server is running on http://localhost:${port}`);
 });
